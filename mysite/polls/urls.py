827f4cf2dce5bfb8e6b978f1c75b988883539060
Yuhua Ni <yuhuani50@gmail.com>
2017-01-20 17:03:57 -0500

    working on part 3 of the django tutorial

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.HelloWorldView.as_view())
+    url(r'^$', views.HelloWorldView.as_view()),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^({<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View, TemplateView
+from django.template import loader
+
+from .models import Question
 # Create your views here.
 
 class HelloWorldView(View):
     
     def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello World!")
+        latest_question_list = Question.objects.order_by('-pub_date')[:5]
+        context = {
+            'latest_question_list': latest_question_list,
+        }
+        return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
